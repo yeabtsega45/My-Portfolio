527a1b6813043a71bb6f44f8b0a14cace62417db
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-07 20:20:16 +0300

    creating navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <Link></Link>
+    </div>
+  );
+}
+
+export default Navbar;
