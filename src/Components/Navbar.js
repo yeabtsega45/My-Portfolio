c1e8ec61eb068f02a063bac11cbd646854e8aff8
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-07 20:33:31 +0300

    adding links to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,6 +8,20 @@ function Navbar() {
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 }
