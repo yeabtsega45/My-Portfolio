e3018bfeed4e11149768773714c390a27a7faaac
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-07 20:11:17 +0300

    adding path to the routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import About from "./Routes/About";
+import Contact from "./Routes/Contact";
+import Home from "./Routes/Home";
+import Project from "./Routes/Project";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
