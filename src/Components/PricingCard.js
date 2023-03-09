7aa0198f2adb0211a497d2eff4787d002d3c0a98
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 22:49:14 +0300

    creating a pricing card component

---
+++ src/Components/PricingCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PricingCard.css";
+
+function PricingCard() {
+  return (
+    <div className="pricing">
+      <div></div>
+    </div>
+  );
+}
+
+export default PricingCard;
