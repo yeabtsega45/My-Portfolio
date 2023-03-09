6ce000560332ac4366aa2f7bcaae7c26420e8da2
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 16:43:03 +0300

    creating reusable frontpage component

---
+++ src/Components/HeroImg2.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HeroImgStyles2.css";
+
+function HeroImg2() {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>text</h1>
+        <p>text text text</p>
+      </div>
+    </div>
+  );
+}
+
+export default HeroImg2;
--- src/Routes/Project.js
+++ src/Routes/Project.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import HeroImg2 from "../Components/HeroImg2";
 
 function Project() {
   return (
     <div>
       <Navbar />
+      <HeroImg2 />
       <Footer />
     </div>
   );
