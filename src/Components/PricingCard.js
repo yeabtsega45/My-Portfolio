7ba0bd422f8fd7876d55d160782ee62344d9f9ec
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 22:54:09 +0300

    working on the pricing card component

--- src/Components/PricingCard.js
+++ src/Components/PricingCard.js
@@ -4,7 +4,13 @@ import "./PricingCard.css";
 function PricingCard() {
   return (
     <div className="pricing">
-      <div></div>
+      <div className="card-container">
+        <div className="card">
+          <h3>- Basic -</h3>
+          <span className="bar"></span>
+          <p className="btc">$ 100</p>
+        </div>
+      </div>
     </div>
   );
 }
