5c5b450360ffc734f083bb616c39d76216f3a9e0
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 15:00:31 +0300

    working on footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,21 @@
 import "./Footer.css";
 import React from "react";
+import { FaHome } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer-container">
-        <div className="left"></div>
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "white", marginRight: "2rem" }} />
+            <div>
+              <p>123 Housing Society.</p>
+              <p>Bangladesh.</p>
+            </div>
+          </div>
+          <div className="phone"></div>
+        </div>
         <div className="right"></div>
       </div>
     </div>
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import HeroImg from "../Components/HeroImg";
+import Footer from "../Components/Footer";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <HeroImg />
+      <Footer />
     </div>
   );
 }
