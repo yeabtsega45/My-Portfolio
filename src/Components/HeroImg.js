7ac0a491fb9b939fffb3fffb9684fbcfc927b1a4
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 12:08:36 +0300

    adding background img

--- src/Components/HeroImg.js
+++ src/Components/HeroImg.js
@@ -1,11 +1,13 @@
 import "./HeroImgStyles.css";
-
 import React from "react";
+import IntroImg from "../assets/IntroImg";
 
 const HeroImg = () => {
   return (
     <div className="hero">
-      <div className="mask"></div>
+      <div className="mask">
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
     </div>
   );
 };
