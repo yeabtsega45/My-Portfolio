7af69e6ba95f7edc7acf796cb9bed2e4ab745e6f
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 16:24:59 +0300

    importing the footer component on the pages

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
 }
 
-export default About
+export default About;
