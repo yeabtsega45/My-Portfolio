83de7ee0ef7f88795104800b10f94852d067d0ae
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 16:15:13 +0300

    importing the footer component on project page

--- src/Routes/Project.js
+++ src/Routes/Project.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 function Project() {
   return (
-    <div>Project</div>
-  )
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
 }
 
-export default Project
+export default Project;
