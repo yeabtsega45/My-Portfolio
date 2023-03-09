a33f2a61d926546696f1a9ec9243586ca5a9f3a1
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 12:01:03 +0300

    creating a component

---
+++ src/Components/HeroImg.js
@@ -0,0 +1,13 @@
+import "./HeroImgStyles.css";
+
+import React from "react";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask"></div>
+    </div>
+  );
+};
+
+export default HeroImg;
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import HeroImg from "../Components/HeroImg";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <HeroImg />
     </div>
   );
 }
