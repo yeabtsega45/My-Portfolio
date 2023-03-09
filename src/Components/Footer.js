6799e789bd21cb7656b0b04670b82d1e12b3252f
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 15:10:38 +0300

    working on footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 import "./Footer.css";
 import React from "react";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -10,11 +10,28 @@ function Footer() {
           <div className="location">
             <FaHome size={20} style={{ color: "white", marginRight: "2rem" }} />
             <div>
-              <p>123 Housing Society.</p>
-              <p>Bangladesh.</p>
+              <p>Addis Ababa.</p>
+              <p>Ethiopia.</p>
             </div>
           </div>
-          <div className="phone"></div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "white", marginRight: "2rem" }}
+              />
+              0912704375
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "white", marginRight: "2rem" }}
+              />
+              yabtsega2022@gmail.com
+            </h4>
+          </div>
         </div>
         <div className="right"></div>
       </div>
