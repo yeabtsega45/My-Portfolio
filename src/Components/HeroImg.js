39d4dfdcfc79ef5ac456bdcea0737f99a0a4d9b9
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-09 12:13:39 +0300

    adding text content to homepage

--- src/Components/HeroImg.js
+++ src/Components/HeroImg.js
@@ -1,12 +1,20 @@
 import "./HeroImgStyles.css";
 import React from "react";
 import IntroImg from "../assets/IntroImg";
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
         <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>HI, I'M A FREELANCER.</p>
+        <h1>React Developer.</h1>
+        <div>
+          <Link></Link>
+        </div>
       </div>
     </div>
   );
