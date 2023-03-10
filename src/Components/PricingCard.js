4820d471e6577bb23424d3126ed39ef680d6e0b1
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-10 09:43:16 +0300

    creating the card component

--- src/Components/PricingCard.js
+++ src/Components/PricingCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./PricingCard.css";
+import { Link } from "react-router-dom";
 
 function PricingCard() {
   return (
@@ -9,6 +10,13 @@ function PricingCard() {
           <h3>- Basic -</h3>
           <span className="bar"></span>
           <p className="btc">$ 100</p>
+          <p>- 3 Days -</p>
+          <p>- 3 Pages -</p>
+          <p>- Featured -</p>
+          <p>- Responsive Design -</p>
+          <Link to="/contact" className="btn">
+            PURCHASE NOW
+          </Link>
         </div>
       </div>
     </div>
