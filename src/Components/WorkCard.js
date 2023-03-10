94dd2b8838a25229f46eb6af834837352537f446
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-10 14:38:04 +0300

    importing project images to the site

--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./WorkCard.css";
+import pro1 from "../assets/project1.png";
 
 function WorkCard() {
   return (
@@ -7,7 +8,7 @@ function WorkCard() {
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
         <div className="project-card">
-          <img src="" alt="image" />
+          <img src={pro1} alt="project-img" />
         </div>
       </div>
     </div>
