21ec9463e7229e199e558476dbee458218cfe353
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-10 14:01:58 +0300

    working on the new card component

--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -3,8 +3,13 @@ import "./WorkCard.css";
 
 function WorkCard() {
   return (
-    <div>
-      <div></div>
+    <div className="work-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-container">
+        <div className="project-card">
+          <img src="" alt="image" />
+        </div>
+      </div>
     </div>
   );
 }
