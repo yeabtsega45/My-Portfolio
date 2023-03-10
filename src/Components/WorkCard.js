a47a03fe54daf5abc75ef7b366c57a139063421b
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-10 10:50:52 +0300

    creating another card component

---
+++ src/Components/WorkCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./WorkCard.css";
+
+function WorkCard() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default WorkCard;
--- src/Routes/Project.js
+++ src/Routes/Project.js
@@ -3,12 +3,14 @@ import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import HeroImg2 from "../Components/HeroImg2";
 import PricingCard from "../Components/PricingCard";
+import WorkCard from "../Components/WorkCard";
 
 function Project() {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
+      <WorkCard />
       <PricingCard />
       <Footer />
     </div>
