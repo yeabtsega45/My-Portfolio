d91b078ff1661b48f8e68d5d219735a1a3206540
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-11 22:32:23 +0300

    using props to import the data

--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -1,20 +1,19 @@
 import React from "react";
 import "./WorkCard.css";
-import pro1 from "../assets/project1.png";
 import { NavLink } from "react-router-dom";
 
-function WorkCard() {
+function WorkCard(props) {
   return (
     <div className="project-card">
-      <img src={pro1} alt="project-img" />
-      <h2 className="project-title">project-title</h2>
+      <img src={props.imgsrc} alt="project-img" />
+      <h2 className="project-title">{props.title}</h2>
       <div className="pro-details">
-        <p>this is text</p>
+        <p>{props.text}</p>
         <div className="pro-btns">
-          <NavLink to="url.com" className="btn">
+          <NavLink to={props.view} className="btn">
             View
           </NavLink>
-          <NavLink to="url.com" className="btn">
+          <NavLink to={props.view} className="btn">
             Source
           </NavLink>
         </div>
