2e98f58a0b3a9a81da04ee6a7456084a4d3b220b
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-13 15:30:34 +0300

    creating a form component

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Form.css";
+
+function Form() {
+  return (
+    <div className="form">
+      <form></form>
+    </div>
+  );
+}
+
+export default Form;
--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import HeroImg2 from "../Components/HeroImg2";
+import Form from "../Components/Form";
 
 function Contact() {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="CONTACT." text="Let's have a chat." />
+      <Form />
       <Footer />
     </div>
   );
