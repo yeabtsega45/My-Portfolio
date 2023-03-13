f48b2906dbe300e6108210aee60d021122eae425
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-13 16:23:49 +0300

    importing the work component on the home page

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import HeroImg from "../Components/HeroImg";
 import Footer from "../Components/Footer";
+import Work from "../Components/Work";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <HeroImg />
+      <Work />
       <Footer />
     </div>
   );
