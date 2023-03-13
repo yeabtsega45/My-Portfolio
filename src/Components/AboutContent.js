c7fa306623e9dd1a800725a4711444094bb1de3e
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-13 14:23:58 +0300

    working on about content

--- src/Components/AboutContent.js
+++ src/Components/AboutContent.js
@@ -3,8 +3,15 @@ import "./AboutContent.css";
 
 function AboutContent() {
   return (
-    <div>
-      <div></div>
+    <div className="about">
+      <div className="left">
+        <h1>Who Am I?</h1>
+        <p>
+          I'm a react front-end developer. I create responsive secure websites
+          for my clients.
+        </p>
+      </div>
+      <div className="right"></div>
     </div>
   );
 }
--- src/Routes/About.js
+++ src/Routes/About.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import HeroImg2 from "../Components/HeroImg2";
+import AboutContent from "../Components/AboutContent";
 
 function About() {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="ABOUT." text="I'm a friendly Front-End Developer." />
+      <AboutContent />
       <Footer />
     </div>
   );
