b049e6168aedc27087b6a1351f897c5102e9324a
yeabtsega45 <yabtsega2022@gmail.com>
2023-08-01 00:18:26 +0300

    creating contact page

--- src/Components/ContactContent.js
+++ src/Components/ContactContent.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "./ContactContent.css";
+import { FaGithub, FaMailBulk, FaPhone } from "react-icons/fa";
 
 function ContactContent() {
   return (
     <div className="contact">
-      <h1>Who Am I?</h1>
-      <p>
-        I'm a fullstack developer. I create responsive secure websites for my
-        clients.
-      </p>
-      <a href="https://github.com/yeabtsega45">
-        <button className="btn">Github</button>
-      </a>
+      <div className="phone">
+        <h4>
+          <FaPhone size={20} style={{ color: "white", marginRight: "2rem" }} />
+          <a href="tel:+251912704375">+251912704375</a>
+        </h4>
+      </div>
+      <div className="email">
+        <h4>
+          <FaMailBulk
+            size={20}
+            style={{ color: "white", marginRight: "2rem" }}
+          />
+          <a href="mailto:yabtsega2022@gmail.com">yabtsega2022@gmail.com</a>
+        </h4>
+      </div>
+      <div className="github">
+        <h4>
+          <FaGithub size={20} style={{ color: "white", marginRight: "2rem" }} />
+          <a href="https://github.com/yeabtsega45">yeabtsega45</a>
+        </h4>
+      </div>
     </div>
   );
 }
