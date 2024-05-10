8e99c964189d4ebb7a0f0eee0e159bb5af8ac39b
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-10 16:05:23 +0300

    update project data

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -22,7 +22,7 @@ function Navbar() {
   return (
     <div className={color ? "header header-bg" : "header"}>
       <Link to="/">
-        <h1>Portfolio</h1>
+        <h1>Yeabtsega</h1>
       </Link>
       <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
