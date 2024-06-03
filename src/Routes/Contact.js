ba3e0aee149f8506743cb67c9c8629a7042000fc
yeabtsega45 <yabtsega2022@gmail.com>
2024-06-03 16:24:00 +0300

    add styling

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -2,15 +2,18 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import HeroImg2 from "../Components/HeroImg2";
-import ContactContent from "../Components/ContactContent";
+// import ContactContent from "../Components/ContactContent";
 // import Form from "../Components/Form";
 
 function Contact() {
   return (
     <div>
       <Navbar />
-      <HeroImg2 heading="CONTACT." text="Let's have a chat." />
-      <ContactContent />
+      <HeroImg2
+        heading="CONTACT."
+        text="I'm up for any new job options. I can be reached using the information below."
+      />
+      {/* <ContactContent /> */}
       {/* <Form /> */}
       <Footer />
     </div>
