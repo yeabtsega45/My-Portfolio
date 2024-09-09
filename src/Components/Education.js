7a578452f47c5154103382c4bfa2d639a574eb68
yeabtsega45 <yabtsega2022@gmail.com>
2024-09-09 22:46:46 +0300

    add Education component

---
+++ src/Components/Education.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Education() {
+  return (
+    <div className="education-container">
+      <h1 className="education-title">Education</h1>
+      <div className="education-list">
+        {educationData.map((edu, index) => (
+          <div key={index} className="education-item">
+            <h2 className="education-degree">{edu.degree}</h2>
+            <h3 className="education-institution">{edu.institution}</h3>
+            <p className="education-year">{edu.year}</p>
+            <p className="education-description">{edu.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Education;
